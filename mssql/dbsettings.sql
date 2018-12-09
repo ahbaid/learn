53239bf77c9db1248d9105acc4c1cf7995bb0737
Ahbaid Gaffoor <ahbaid.gaffoor@elliemae.com>
2018-12-09 14:14:55 -0800

    updates

--- mssql/dbsettings.sql
+++ mssql/dbsettings.sql
@@ -6,5 +6,10 @@ Date:		Sunday Dec 9th 2018
 use master
 go
 
+-- Show database version
+select @@version
+go
+
+-- Check if query store is enabled
 select name, is_query_store_on from sys.databases
 go
