068fd9d781f880f84f995679467ef948feafeb1f
Ahbaid Gaffoor <ahbaid@gmail.com>
2019-02-07 23:27:51 -0800

    smile

--- python/args/pargs.py
+++ python/args/pargs.py
@@ -4,10 +4,32 @@ import argparse
 
 # Create a parser
 ap = argparse.ArgumentParser()
-ap.add_argument("x", help="enter a value for x")
-ap.add_argument("y", help="enter a value for y")
+
+# Add two required args, of type int
+ap.add_argument("x", help="enter a value for x", type=int)
+ap.add_argument("y", help="enter a value for y", type=int)
+
+# Add an option arg, used as a signal or flag, with short and long forms
+ap.add_argument("-v", "--verbose", help="be verbose", action="store_true")
+
+# Add an arg, optional, but with restricted choices
+ap.add_argument("-s", "--smiles", type=int, choices=[1,2,3], help="how many smiley faces to show")
+
+# Parse the args
 ap.parse_args()
 
 args=ap.parse_args()
-print("Value of x was %s" % args.x)
-print("Value of y was %s" % args.y)
+sum=args.x+args.y
+
+if args.verbose:
+   print("The value of x is %s" % args.x)
+   print("The value of y is %s" % args.y)
+   print("The sum of {} + {} is {}".format(args.x,args.y,sum))
+else:
+   print("x=%s" % args.x)
+   print("y=%s" % args.y)
+   print("{}+{}={}".format(args.x,args.y,sum))
+
+if args.smiles:
+   for i in range(0,args.smiles):
+      print(":)")
