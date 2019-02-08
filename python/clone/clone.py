bf9f6977415c4f45c25f8243fc68365d39984310
Ahbaid Gaffoor <ahbaid@gmail.com>
2019-02-07 22:47:42 -0800

    init

---
+++ python/clone/clone.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import os
+import argparse
+
+def main():
+
+   # Constants to replace
+   replace_jira = "JJJJJ"
+   replace_change_request = "CCCCC"
+
+
+   
+
+if __name__ == "__main__":
+   main()
