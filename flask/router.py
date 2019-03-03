4c84c06a29706eb3aa05fd269308de0c44255e85
Ahbaid Gaffoor <ahbaid@gmail.com>
2019-03-03 15:57:48 -0800

    updated files

--- flask/router.py
+++ flask/router.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
    return render_template("index.html")
 
+@app.route("/more")
+def more():
+   return render_template("more.html")
+
 if __name__ == "__main__":
    app.run(debug=True)
 
