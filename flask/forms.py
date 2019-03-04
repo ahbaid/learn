c8c780b699bcbb0cf955e267c26c81903330b90e
Ahbaid Gaffoor <ahbaid@gmail.com>
2019-03-03 21:23:24 -0800

    heroku

--- flask/forms.py
+++ flask/forms.py
@@ -1,9 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, validators
 
-# FlaskWTFDeprecationWarning: "flask_wtf.Form" has been renamed to "FlaskForm" and will be removed in 1.0.
+# {{{ FlaskWTFDeprecationWarning: "flask_wtf.Form" has been renamed to "FlaskForm" and will be removed in 1.0.
+#
+# from flask_wtf import Form
+# changes to
+# from flask_wtf import FlaskForm
+#
+# class AddUserForm(Form):
+# changes to
+# class AddUserForm(FlaskForm):
+# }}}
 
 class AddUserForm(FlaskForm):
-   username = StringField('Username')
-   password = PasswordField('Password')
+   username = StringField('Username', [validators.DataRequired(message="Please enter a username.")])
+   password = PasswordField('Password', [validators.DataRequired("Please enter a password.")])
    submit = SubmitField('Add User')
--- flask/router.py
+++ flask/router.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from models import db
 from forms import AddUserForm
 
@@ -9,7 +9,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
 db.init_app(app)
 
 # Protect against CSRF (Cross Site Request Forgery) attacks
-app.secret_key = "development-key"
+app.secret_key = "c1c673e3-5503-4885-8471-5ce78a5a32e8"
 
 @app.route("/")
 def index():
@@ -19,10 +19,16 @@ def index():
 def more():
    return render_template("more.html")
 
-@app.route("/adduser")
+@app.route("/adduser", methods=['GET','POST'])
 def adduser():
    form = AddUserForm()
-   return render_template("adduser.html", form=form)
+   if request.method == 'POST':
+      if form.validate() == False:
+         return render_template("adduser.html", form=form)
+      else:
+         return "Success!"
+   elif request.method == 'GET':
+      return render_template("adduser.html", form=form)
 
 if __name__ == "__main__":
    app.run(debug=True)
