296c709da77991d4ee573f1706e985ee7b844c98
ahbaidg <ahbaid@gmail.com>
2019-03-24 11:29:36 -0700

    dmidecode

--- linux/dmidecode/dmidecode.sh
+++ linux/dmidecode/dmidecode.sh
@@ -1,12 +1,32 @@
 #!/bin/bash 
-echo "# {{{ Motherboard"
-dmidecode -s baseboard-product-name
-dmidecode -t baseboard
+
+UL="================================================"
+
+echo -e "# {{{ Motherboard"
+
+echo -e "\n# Manufacturer\n$UL"
 dmidecode -s baseboard-manufacturer
-echo "# }}}"
-echo "# {{{ Motherboard"
+
+echo -e "\n# Product Name\n$UL"
+dmidecode -s baseboard-product-name
+
+echo -e "\n# Motherboard\n$UL"
+dmidecode -t baseboard
+
+echo -e "# }}}"
+
+echo -e "# {{{ Processor"
+
+echo -e "\n# Processor Family\n$UL"
 dmidecode -s processor-family
+
+echo -e "\n# Processor Manufacturer\n$UL"
 dmidecode -s processor-manufacturer
+
+echo -e "\n# Processor Version\n$UL"
 dmidecode -s processor-version
+
+echo -e "\n# Processort Frequency\n$UL"
 dmidecode -s processor-frequency
-echo "# }}}"
+
+echo -e "# }}}"
