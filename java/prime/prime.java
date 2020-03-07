73919e9ad1525dbe30caa448cc3098ccac346358
Ahbaid Gaffoor <ahbaid@gmail.com>
2020-03-07 13:16:51 -0800

    updated code for prime.java

--- java/prime/prime.java
+++ java/prime/prime.java
@@ -1,50 +1,77 @@
 import java.util.Scanner;
-public class prime
-{
+public class prime {
    
-    public static void banner() 
-    {
+    public static void banner() {
        System.out.printf("\nPrime Generator v0.1\n");
        System.out.printf("=============================\n");
     }
 
-    public static int get_number(String s)
-    {
+    public static char get_char(String s) {
        System.out.print(s);
-       Scanner input = new Scanner (System.in);
-       return input.nextInt();
-    }
- 
-    public static String is_prime(int n)
-    {
-       if (n==2 || n==1) {
-          return "PRIME";
-       } else if ((n % 2)==0) {
-          return "Even";
-       } else {
-          return "Odd, Could be prime.... more code please.....";
-       }
+       Scanner Keyboard = new Scanner (System.in);
+       return Keyboard.next().charAt(0);
     }
 
-    public static void get_primes(int nprimes, int startn)
-    {
-       int i=1;
-       int evaln=startn;
-       while (i<=nprimes)
-       {
-           System.out.printf("%02d. Evaluating %2d : ",i,evaln);
-           System.out.printf("%s\n",is_prime(evaln));
-           i++;
-           evaln+=1;
-       }
+    public static int get_number(String s) {
+       System.out.print(s);
+       Scanner Keyboard = new Scanner (System.in);
+       return Keyboard.nextInt();
     }
  
-    public static void main(String args[])
-    {
+    public static boolean is_prime(int n) {
+       if (n==2 || n==1) {
+          return true;
+       } 
+       else if ((n % 2)==0) {
+          return false;
+       }
+       return false;
+    }
+
+    public static void get_primes(int nprimes, int startn, char verbose) {
+
+       int i=1;
+       int evaln=startn;
+       boolean foundOne=false;
+
+       if (verbose=='y') {
+          System.out.printf("\nEvaluating from %d -> %d showing all results...\n\n",startn,startn+nprimes);
+       } else {
+          System.out.printf("\nEvaluating from %d -> %d showing only prime results...\n\n",startn,startn+nprimes);
+       }
+
+       while (i<=nprimes) {
+
+           if (is_prime(evaln)) {
+              foundOne=true;
+              if (verbose=='y') {
+                  System.out.printf("%02d. Evaluated %3d as PRIME\n",i,evaln);
+              } else {
+                  System.out.printf("%d,",evaln);
+              }
+           } else {
+              if (verbose=='y') {
+                  System.out.printf("%02d. Evaluated %3d as NOT-PRIME\n",i,evaln);
+              } 
+           }
+
+           i++;
+           evaln+=1;
+
+       }
+
+       if (foundOne) {
+          System.out.printf("\n");
+       }
+
+    }
+ 
+    public static void main(String args[]) {
         banner();
-        int n = get_number("How many primes? ");
-        int s = get_number("Integer to start from? ");
-        get_primes(n,s);
+        int  n = get_number("How many primes? ");
+        int  s = get_number("Integer to start from? ");
+        char v = get_char("Show non-primes(y/n)? ");
+        get_primes(n,s,v);
         System.out.printf("\n");
     }
    
