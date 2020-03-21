8d6344ee177f80868e412b4fb9e80807494bf2b0
ahbaidg <ahbaid@gmail.com>
2020-03-21 16:10:00 -0700

    simplemath

--- C++/simplemath/main.cpp
+++ C++/simplemath/main.cpp
@@ -2,8 +2,8 @@
 #include "simplemath.h"
 
 int main() {
-   std:cout << "Simple Math Demo\n";
-   std:cout << "================\n";
+   std::cout << "Simple Math Demo\n";
+   std::cout << "================\n";
    std::cout << "The sum of 3 and 4 is: " << sum(3,4) << "\n";
    return 0;
 }
